6fa41f91cd60c7b785403133a2480d827b6bbb8a
unknown <kathleentonner@gmail.com>
2020-11-04 12:59:16 -0500

    last commit before trying to publish

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Nav from 'react-bootstrap/Nav'
 import { NavLink } from 'react-router-dom'
 import './App.css';
 import Navigation from './Components/Navbar'
+
 import Home from './pages/Home/Home'
 import Work from './pages/Work/Work'
 import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
